be29d0d8dafae1f1d1800050baa3a216b823235c
jtpennell <jayt24seven@gmail.com>
2019-07-10 00:08:02 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function fib(x) {
   if (x === 1) {
-    return [0,1];
+    return "1"; // first sequence without zero
   } else {
-    var n = fib(x - 1);
-    n.push(n[n.length - 1] + n[n.length - 2]);
-    return n;
+    var n = [0,1];
+    for (i = 2; i < x; i++){
+      n.push(n[n.length - 1] + n[n.length - 2]); // fib sequence formula
+    }
+    n = n.slice(1,n.length); // slice list removing 0 from sequence
+    return n.join(", "); // formatting to the specifications
   }
 }
 
 var x = 6;
-console.log(fib(x));
+console.log(fib(x)); // should output '1, 1, 2, 3, 5'
